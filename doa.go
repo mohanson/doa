44d367da3ecbaed49a629a2f811e9892d27a7b79
mohanson <mohanson@outlook.com>
2021-01-10 14:02:52 +0800

    Add assert keyword

--- doa.go
+++ doa.go
@@ -1,11 +1,27 @@
 package doa
 
+// Doa1 test if a condition returns true
+func Doa1(a bool) {
+	if !a {
+		panic("assertion error")
+	}
+}
+
+// Doa2 likes Doa1.
+func Doa2(a bool, b bool) {
+	if !(a || b) {
+		panic("assertion error")
+	}
+}
+
+// Try1 panics when it receives a error.
 func Try1(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
+// Try2 panics when it receives a error.
 func Try2(a interface{}, err error) interface{} {
 	if err != nil {
 		panic(err)
@@ -13,6 +29,7 @@ func Try2(a interface{}, err error) interface{} {
 	return a
 }
 
+// Try3 panics when it receives a error.
 func Try3(a interface{}, b interface{}, err error) (interface{}, interface{}) {
 	if err != nil {
 		panic(err)
@@ -20,6 +37,7 @@ func Try3(a interface{}, b interface{}, err error) (interface{}, interface{}) {
 	return a, b
 }
 
+// Try4 panics when it receives a error.
 func Try4(a interface{}, b interface{}, c interface{}, err error) (interface{}, interface{}, interface{}) {
 	if err != nil {
 		panic(err)
